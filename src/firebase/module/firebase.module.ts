04d1a3d7c891e4538c06fa6e961b939e1cca957e
Antonio Barrios <93525722+nfuegopy@users.noreply.github.com>
2024-06-07 14:30:04 -0400

    Update firebase.module.ts

--- src/firebase/module/firebase.module.ts
+++ src/firebase/module/firebase.module.ts
@@ -4,25 +4,36 @@ import { FirebaseService } from '../service/firebase.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [ConfigModule],
-    providers: [
-        FirebaseService,
-        {
-            provide: 'FIREBASE_ADMIN',
-            useFactory: (configService: ConfigService) => {
-                const adminConfig = {
-                    credential: admin.credential.cert({
-                        projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
-                        privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
-                        clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-                    }),
-                    databaseURL: configService.get<string>('FIREBASE_DATABASE_URL'),
-                };
-                return admin.initializeApp(adminConfig);
-            },
-            inject: [ConfigService],
-        },
-    ],
-    exports: ['FIREBASE_ADMIN', FirebaseService],
+  imports: [ConfigModule.forRoot()],
+  providers: [
+    FirebaseService,
+    {
+      provide: 'FIREBASE_ADMIN',
+      useFactory: (configService: ConfigService) => {
+        const projectId = configService.get<string>('FIREBASE_PROJECT_ID');
+        const privateKey = configService.get<string>('FIREBASE_PRIVATE_KEY')?.replace(/\\n/g, '\n');
+        const clientEmail = configService.get<string>('FIREBASE_CLIENT_EMAIL');
+
+        console.log('FIREBASE_PROJECT_ID:', projectId);
+        console.log('FIREBASE_PRIVATE_KEY:', privateKey);
+        console.log('FIREBASE_CLIENT_EMAIL:', clientEmail);
+
+        if (!projectId || !privateKey || !clientEmail) {
+          throw new Error('Firebase environment variables are not set correctly');
+        }
+
+        const adminConfig = {
+          credential: admin.credential.cert({
+            projectId: projectId,
+            privateKey: privateKey,
+            clientEmail: clientEmail,
+          }),
+        };
+        return admin.initializeApp(adminConfig);
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: ['FIREBASE_ADMIN', FirebaseService],
 })
-export class FirebaseModule { }
+export class FirebaseModule {}

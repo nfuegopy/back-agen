0302a94d77be47e7dfd28e8c65319b89604e1d3e
Antonio Barrios <acbleguizamon@gmail.com>
2024-06-08 07:33:11 -0400

    Subida desde branch 3 con el cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
-  const port = process.env.PORT || 3000; // Obtener el puerto de la variable de entorno o usar 3000 por defecto
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();

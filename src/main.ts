40593264451e9770b17a72b1481cdc828bf50eaf
Antonio Barrios <93525722+nfuegopy@users.noreply.github.com>
2024-06-08 07:46:48 -0400

    Update main.ts
    update en el main

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configurar CORS
   app.enableCors({
-    origin: '*',//nueva subida
+    origin: '*', // Permitir solicitudes desde cualquier origen
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
   const configService = app.get(ConfigService);
-  const port = process.env.PORT || 3000;
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const port = configService.get<number>('PORT') || 3000; // Obtener el puerto de la variable de entorno o usar 3000 por defecto
+  await app.listen(port);
+
+  const appUrl = await app.getUrl(); // Obtener la URL después de que la aplicación esté escuchando
+  console.log(`Application is running on: ${appUrl}`);
 }
+
 
 bootstrap();
